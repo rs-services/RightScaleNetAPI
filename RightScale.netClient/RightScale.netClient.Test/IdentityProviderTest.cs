2e528d8d80aadd1ea66c9c8278ac21bc57932f77
Patrick McClory <pmcclory@gmail.com>
2013-03-07 10:54:17 -0500

    Adding IdentityProviderTest stub

---
+++ RightScale.netClient/RightScale.netClient.Test/IdentityProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RightScale.netClient.Test
+{
+    [TestClass]
+    public class IdentityProviderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
