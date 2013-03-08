c3bfa68ed99b130584d464651958be93408294f1
Patrick McClory <pmcclory@gmail.com>
2013-03-07 22:27:46 -0500

    Lots of documentation
    closes #73, much neede docmentation added

--- RightScale.netClient/RightScale.netClient/Core/RightScaleObjectBase.cs
+++ RightScale.netClient/RightScale.netClient/Core/RightScaleObjectBase.cs
@@ -7,9 +7,22 @@ using Newtonsoft.Json;
 
 namespace RightScale.netClient.Core
 {
+    /// <summary>
+    /// Base class for all RS-based API objects.  Centralizes logic for links and actions and handles constructor logic for authentication stuff
+    /// </summary>
+    /// <typeparam name="T">Type of the derived class--used to manage proper serialization</typeparam>
     public class RightScaleObjectBase<T>
     {
+        /// <summary>
+        /// Generic collection of Action objects accompanying a given RightScale API Object
+        /// Defines what actions are currently possible on the current object instance
+        /// </summary>
         public List<Action> actions { get; set; }
+        
+        /// <summary>
+        /// Generic collection of Link objects accompanying a given RightScale API Object
+        /// Links contain references to other sets of objects and are where object ID's are derived from
+        /// </summary>
         public List<Link> links { get; set; }
 
         /// <summary>
@@ -27,6 +40,14 @@ namespace RightScale.netClient.Core
             return idToReturn.Last<string>().Split('/').Last<string>();
         }
 
+        #region RightScaleObjectBase.ctor
+
+        /// <summary>
+        /// RightScaleObjectBase constructor for authenticating via Username, password and accountno
+        /// </summary>
+        /// <param name="userName">emmail address for logging into the RightScale API</param>
+        /// <param name="password">password for the given account used for authenticating to the RightScale API</param>
+        /// <param name="accountNo">Account Number to perform API actions against</param>
         public RightScaleObjectBase(string userName, string password, string accountNo)
         {
             APIClient.Instance.userName = userName;
@@ -34,38 +55,74 @@ namespace RightScale.netClient.Core
             APIClient.Instance.accountId = accountNo;
         }
 
+        /// <summary>
+        /// RightScaleObjectBase constructor for authenticating via oAuth2
+        /// </summary>
+        /// <param name="oAuthRefreshToken">RightScale oAuth2 refresh token</param>
         public RightScaleObjectBase(string oAuthRefreshToken)
         {
             APIClient.Instance.oauthRefreshToken = oAuthRefreshToken;
         }
 
+        /// <summary>
+        /// RightScaleObjectBase default constructor.  By using this constructor it is implied that authentication information will be placed in the application's configuration file
+        /// </summary>
         public RightScaleObjectBase()
         {
 
         }
 
+        #endregion
+
         #region Json deserialization helpers
 
+        /// <summary>
+        /// Method deserializes a Json string into a single instance of the derived class defined by <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="jsonString">Json String to deserialize into an instance of <typeparamref name="T"/></param>
+        /// <returns>Instance of type <typeparamref name="T"/></returns>
         protected static T deserialize(string jsonString)
         {
             return objectDeserialize(jsonString);
         }
 
+        /// <summary>
+        /// Method deserializes a Json string into a generic list of objects of the derived class defined by <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <returns>Generic list of <typeparamref name="T"/></returns>
         protected static List<T> deserializeList(string jsonString)
         {
             return listDeserialize(jsonString);
         }
 
-        public static T populateObjectFromJson(string jsonString)
+        /// <summary>
+        /// Method deserializes a Json string into a single instance of the derived class defined by <typeparamref name="T"/>
+        /// This method is accessible by other classes in this library for classes that return collections of other types of classes
+        /// </summary>
+        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <returns></returns>
+        internal static T populateObjectFromJson(string jsonString)
         {
             return objectDeserialize(jsonString);
         }
 
-        public static List<T> populateObjectListFromJson(string jsonString)
+        /// <summary>
+        /// Method deserializes a Json string into a generic list of objects of the derived class defined by <typeparamref name="T"/>
+        /// This method is accessible by other classes in this library for classes that return collections of other types of classes
+        /// </summary>
+        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <returns></returns>
+        internal static List<T> populateObjectListFromJson(string jsonString)
         {
             return listDeserialize(jsonString);
         }
 
+        /// <summary>
+        /// Centralized private method for handling deserialization of Json into the proper generic list of type defined by <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <returns>Generic list of type <typeparamref name="T"/></returns>
         private static List<T> listDeserialize(string jsonString)
         {
             try
@@ -78,6 +135,11 @@ namespace RightScale.netClient.Core
             }
         }
 
+        /// <summary>
+        /// Centralized private method for handling deserialization of Json into the proper type defined by <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="jsonString">Json string to deserizlize into an instance of <typeparamref name="T"/></param>
+        /// <returns>Instance of type <typeparamref name="T"/></returns>
         private static T objectDeserialize(string jsonString)
         {
             try
