00ae92bd8ff81e92c4c42ef8a58232ae032426dc
Michael Peterson <michael.peterson@rightscale.com>
2013-03-22 07:37:19 -0500

    Added session test - not done

---
+++ RightScale.netClient/RightScale.netClient.Test/SessionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace RightScale.netClient.Test
+{
+    [TestClass]
+    public class SessionTest
+    {
+
+        public SessionTest()
+        {
+        }
+        
+        #region Cloud.index tests
+        [TestMethod]
+        public void SessionIndexSimple()
+        {
+            List<Session> listofSessions = Session.index();
+            
+            Assert.IsNotNull(listofSessions);
+            //Assert.IsTrue(listofSessions.count > 0);
+        }
+        #endregion
+    }
+}
