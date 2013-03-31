9fc5b5650e33d6a636561c3b549d505e6ebd5559
Patrick McClory <pmcclory@gmail.com>
2013-03-30 21:12:07 -0400

    another VolumeAttachment.index test
    need to get some real vol attachments in the mix to test this though...
    gonna have to set up cinder locally

--- RightScale.netClient/RightScale.netClient.Test/VolumeAttachmentTest.cs
+++ RightScale.netClient/RightScale.netClient.Test/VolumeAttachmentTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Web;
 
 namespace RightScale.netClient.Test
 {
@@ -9,17 +10,32 @@ namespace RightScale.netClient.Test
     public class VolumeAttachmentTest
     {
         private string volumeAttachmentCloudID;
+        private string cloudID;
+        private string apiRefreshToken;
 
         public VolumeAttachmentTest()
         {
             volumeAttachmentCloudID = ConfigurationManager.AppSettings["VolumeAttachmentTest_cloudID"].ToString();
+            cloudID = HttpUtility.UrlDecode(ConfigurationManager.AppSettings["VolumeType_raxID"].ToString());
+            apiRefreshToken = ConfigurationManager.AppSettings["RightScaleServicesAPIRefreshToken"].ToString();
         }
 
         [TestMethod]
-        public void volumeAttachmentIndex()
+        public void volumeAttachmentIndexSimple()
         {
             List<VolumeAttachment> volAttachments = VolumeAttachment.index(volumeAttachmentCloudID);
             Assert.IsNotNull(volAttachments);
+        }
+
+        //TODO: need to set up cinder locally
+        //[TestMethod]
+        public void volumeAttachmentIndexComplicated()
+        {
+            netClient.Core.APIClient.Instance.InitWebClient();
+            netClient.Core.APIClient.Instance.Authenticate(apiRefreshToken);
+            List<VolumeAttachment> volAttachments = VolumeAttachment.index(cloudID);
+            //Assert.IsNotNull(volAttachments);
+            netClient.Core.APIClient.Instance.InitWebClient();
         }
     }
 }
