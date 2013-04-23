21ab4e89867af440444718ed60e057675688da9b
Patrick McClory <pmcclory@gmail.com>
2013-04-23 00:11:04 -0400

    Adding Wix Install project
    WIX Installer added... need to install WIX 3.7

---
+++ RightScale.PowerShell/RightScale.PowerShell.Install/RightScale.PowerShell.ps1
@@ -0,0 +1,5 @@
+﻿cd 'c:\program files (x86)\rightscale\powershell'; 
+Import-Module .\RightScale.netClient.PowerShell.dll; 
+Get-Content .\WelcomeMessage.txt; 
+$VerbosePreference=\"Continue\"; 
+cd 'C:\'
