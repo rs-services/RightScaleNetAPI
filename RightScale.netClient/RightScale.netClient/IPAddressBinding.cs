8ac3db3263f91a4516e9e0e099e63db591c736fe
Patrick McClory <pmcclory@gmail.com>
2013-04-24 10:57:03 -0400

    IPAddressBinding.create and delete
    resolves #13 - ipaddressbinding create and delete methods implemented

--- RightScale.netClient/RightScale.netClient/IPAddressBinding.cs
+++ RightScale.netClient/RightScale.netClient/IPAddressBinding.cs
@@ -165,5 +165,79 @@ namespace RightScale.netClient
         }
 
         #endregion
+
+        #region IPAddressBinding.create methods
+
+        /// <summary>
+        /// Creates an ip address binding which attaches a specified IpAddress resource to a specified instance, and also allows for configuration of port forwarding rules. If the instance specified is a current (running) instance, a one-time IpAddressBinding will be created. If the instance is a next instance, then a recurring IpAddressBinding is created, which will cause the IpAddress to be bound each time the incarnator boots.
+        /// </summary>
+        /// <param name="cloudID">ID of the Cloud where the IP Address Binding is to be created</param>
+        /// <param name="ipAddressID">ID of the IP Address for this IPAddressBinding</param>
+        /// <param name="instanceID">ID of the instance to be bound to the specified IP Address</param>
+        /// <returns>ID of the newly created IPAddressBinding</returns>
+        public static string create(string cloudID, string ipAddressID, string instanceID)
+        {
+            return create(cloudID, ipAddressID, instanceID);
+        }
+
+        /// <summary>
+        /// Creates an ip address binding which attaches a specified IpAddress resource to a specified instance, and also allows for configuration of port forwarding rules. If the instance specified is a current (running) instance, a one-time IpAddressBinding will be created. If the instance is a next instance, then a recurring IpAddressBinding is created, which will cause the IpAddress to be bound each time the incarnator boots.
+        /// </summary>
+        /// <param name="cloudID">ID of the Cloud where the IP Address Binding is to be created</param>
+        /// <param name="ipAddressID">ID of the IP Address for this IPAddressBinding</param>
+        /// <param name="instanceID">ID of the instance to be bound to the specified IP Address</param>
+        /// <param name="publicPort">public port for mapping incoming NAT rules</param>
+        /// <param name="privatePort">Port to bind on the private side of the network</param>
+        /// <param name="protocol">TCP or UDP protocol specifier</param>
+        /// <returns>ID of the newly created IPAddressBinding</returns>
+        public static string create(string cloudID, string ipAddressID, string instanceID, string publicPort, string privatePort, string protocol)
+        {
+            string instanceHref = string.Format(APIHrefs.InstanceByID, cloudID, instanceID);
+            string ipAddressHref = string.Format(APIHrefs.IPAddressByID, cloudID, instanceID);
+
+            string postString = string.Format(@"/api/clouds/{0}/ip_addresses/{1}/ip_address_bindings", cloudID, instanceID);
+            List<KeyValuePair<string, string>> postParams = new List<KeyValuePair<string, string>>();
+            List<string> validProtocols = new List<string>() { "UDP", "TCP" };
+            Utility.addParameter(instanceHref, "ip_address_binding[instance_href]", postParams);
+            Utility.addParameter(privatePort, "ip_address_binding[private_port]", postParams);
+            Utility.addParameter(protocol, "ip_address_binding[protocol]", postParams);
+            Utility.addParameter(ipAddressHref, "ip_address_binding[public_ip_address_href]", postParams);
+            Utility.addParameter(publicPort, "ip_address_binding[public_port]", postParams);
+            
+            string outString = string.Empty;
+            return Core.APIClient.Instance.Post(postString, postParams, "location").Last<string>().Split('/').Last<string>();
+        }
+
+
+        #endregion
+
+        #region IPAddressBinding.destroy methods
+
+        /// <summary>
+        /// Deletes a specified IPAddressBinding 
+        /// </summary>
+        /// <param name="cloudID">ID of cloud where IPAddressBinding is located</param>
+        /// <param name="ipAddressBindingID">ID of IPAddressBinding to delete</param>
+        /// <param name="ipAddressID">ID of IPAddress associated with the IPAddressBinding to delete</param>
+        /// <returns>true if deleted, false if not</returns>
+        public static bool destroy(string cloudID, string ipAddressBindingID, string ipAddressID)
+        {
+            string deleteHref = string.Format(@"/api/clouds/{0}/ip_addresses/{1}/ip_address_bindings/{2}", cloudID, ipAddressID, ipAddressBindingID);
+            return Core.APIClient.Instance.Delete(deleteHref);
+        }
+
+        /// <summary>
+        /// Deletes a specified IPAddressBinding
+        /// </summary>
+        /// <param name="cloudID">ID of cloud where IPAddressBinding is located</param>
+        /// <param name="ipAddressBindingID">ID of IPAddressBinding to delete</param>
+        /// <returns>true if deleted, false if not</returns>
+        public static bool destroy(string cloudID, string ipAddressBindingID)
+        {
+            string deleteHref = string.Format(@"/api/clouds/{0}/ip_address_bindings/{1}", cloudID, ipAddressBindingID);
+            return Core.APIClient.Instance.Delete(deleteHref);
+        }
+
+        #endregion
     }
 }
