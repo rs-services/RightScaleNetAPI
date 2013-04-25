7b5abc46cbfb878a82efa09caa615a7dfa21b0cb
Patrick McClory <pmcclory@gmail.com>
2013-04-25 06:21:34 -0400

    SSHKey created
    closes #34 - created implementation for cr_d on SSHKey

--- RightScale.netClient/RightScale.netClient/SshKey.cs
+++ RightScale.netClient/RightScale.netClient/SshKey.cs
@@ -48,22 +48,74 @@ namespace RightScale.netClient
         
         #region SshKey.index methods
 
-        public static List<SshKey> index()
+        /// <summary>
+        /// Lists ssh keys.
+        /// </summary>
+        /// <param name="cloudID">ID of cloud to query</param>
+        /// <returns>List of SSHKey objects</returns>
+        public static List<SshKey> index(string cloudID)
         {
-            return index(null, null);
+            return index(cloudID, null, null);
         }
 
-        public static List<SshKey> index(List<Filter> filter)
+        /// <summary>
+        /// Lists ssh keys.
+        /// </summary>
+        /// <param name="cloudID">ID of cloud to query</param>
+        /// <param name="filter">Set of filters to limit return data set</param>
+        /// <returns>List of SSHKey objects</returns>
+        public static List<SshKey> index(string cloudID, List<Filter> filter)
         {
-            return index(filter, null);
+            return index(cloudID ,filter, null);
         }
 
-        public static List<SshKey> index(string view)
+        /// <summary>
+        /// Lists ssh keys.
+        /// </summary>
+        /// <param name="cloudID">ID of cloud to query</param>
+        /// <param name="view">Specifies how many attributes and/or expanded nested relationships to include</param>
+        /// <returns>List of SSHKey objects</returns>
+        public static List<SshKey> index(string cloudID, string view)
         {
-            return index(null, view);
+            return index(cloudID, null, view);
         }
 
-        public static List<SshKey> index(List<Filter> filter, string view)
+        /// <summary>
+        /// Lists ssh keys.
+        /// </summary>
+        /// <param name="cloudID">ID of cloud to query</param>
+        /// <param name="filter">Set of filters to limit return data set</param>
+        /// <param name="view">Specifies how many attributes and/or expanded nested relationships to include</param>
+        /// <returns>List of SSHKey objects</returns>
+        public static List<SshKey> index(string cloudID, List<Filter> filter, string view)
+        {
+            view = validView(view);
+
+            List<string> validFilters = new List<string>() { "resource_uid" };
+            Utility.CheckFilterInput("filter", validFilters, filter);
+
+            string queryString = string.Empty;
+            if (filter != null && filter.Count > 0)
+            {
+                foreach (Filter f in filter)
+                {
+                    queryString += f.ToString() + "&";
+                }
+            }
+
+            queryString += string.Format("view={0}", view);
+            string getHref = string.Format(APIHrefs.SshKey, cloudID);
+            string jsonString = Core.APIClient.Instance.Get(getHref, queryString);
+            return deserializeList(jsonString);
+
+        }
+
+        /// <summary>
+        /// Internal helper method to get valid view string
+        /// </summary>
+        /// <param name="view">View string to test</param>
+        /// <returns>Valid view string for Ssh Key object</returns>
+        private static string validView(string view)
         {
             if (string.IsNullOrWhiteSpace(view))
             {
@@ -74,13 +126,73 @@ namespace RightScale.netClient
                 List<string> validViews = new List<string>() { "default" };
                 Utility.CheckStringInput("view", validViews, view);
             }
-
-            List<string> validFilters = new List<string>() { "resource_uid" };
-            Utility.CheckFilterInput("filter", validFilters, filter);
-
-            //TODO: implement SshKey.index
-            throw new NotImplementedException();
+            return view;
         }
+        #endregion
+
+        #region SshKey.show methods
+
+        /// <summary>
+        /// Displays information about a single Ssh key
+        /// </summary>
+        /// <param name="cloudID">ID of cloud to query</param>
+        /// <param name="sshKeyID">ID of SshKey to return</param>
+        /// <returns>Single populated instance of SshKey object</returns>
+        public static SshKey show(string cloudID, string sshKeyID)
+        {
+            return show(cloudID, sshKeyID, null);
+        }
+
+        /// <summary>
+        /// Displays information about a single Ssh key
+        /// </summary>
+        /// <param name="cloudID">ID of cloud to query</param>
+        /// <param name="sshKeyID">ID of SshKey to return</param>
+        /// <param name="view">Specifies how many attributes and/or expanded nested relationships to include</param>
+        /// <returns>Single populated instance of SshKey object</returns>
+        public static SshKey show(string cloudID, string sshKeyID, string view)
+        {
+            view = validView(view);
+            string getHref = string.Format(APIHrefs.SshKeyByID, cloudID, sshKeyID);
+            string queryString = string.Format("view={0}", view);
+            string jsonString = Core.APIClient.Instance.Get(getHref, queryString);
+            return deserialize(jsonString);
+        }
+
+        #endregion
+
+        #region SshKey.create methods
+
+        /// <summary>
+        /// Creates a new ssh key
+        /// </summary>
+        /// <param name="cloudID">ID of cloud in which to create SSH key</param>
+        /// <param name="name">name of Ssh key</param>
+        /// <returns>ID of newly created SSH key</returns>
+        public static string create(string cloudID, string name)
+        {
+            string postHref = string.Format(APIHrefs.SshKey, cloudID);
+            List<KeyValuePair<string, string>> postParams = new List<KeyValuePair<string, string>>();
+            Utility.addParameter(name, "ssh_key[name]", postParams);
+            return Core.APIClient.Instance.Post(postHref, postParams, "location").Last<string>().Split('/').Last<string>();
+        }
+
+        #endregion
+
+        #region SshKey.destroy methods
+
+        /// <summary>
+        /// Deletes a given ssh key
+        /// </summary>
+        /// <param name="cloudID">ID of cloud where SSH key exists</param>
+        /// <param name="sshKeyID">ID of SSH key to delete</param>
+        /// <returns>true if deleted, false if not</returns>
+        public static bool destroy(string cloudID, string sshKeyID)
+        {
+            string deleteHref = string.Format(APIHrefs.SshKeyByID, cloudID, sshKeyID);
+            return Core.APIClient.Instance.Delete(deleteHref);
+        }
+
         #endregion
     }
 }
