c7482da406ecf567abc1a5dee8c7cf6a92bdc593
Patrick McClory <pmcclory@gmail.com>
2013-05-07 10:05:43 -0400

    adding update to comments for xml deserialization

--- RightScale.netClient/RightScale.netClient/Core/ITaggableResource.cs
+++ RightScale.netClient/RightScale.netClient/Core/ITaggableResource.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace RightScale.netClient.Core
 {
+    /// <summary>
+    /// Common interface for all objects that are Taggable
+    /// </summary>
     public interface ITaggableResource
     {
+        /// <summary>
+        /// Method required to handle populating a taggable resource
+        /// </summary>
         void populateObject();
     }
 }
--- RightScale.netClient/RightScale.netClient/Core/RightScaleAPI10ObjectBase.cs
+++ RightScale.netClient/RightScale.netClient/Core/RightScaleAPI10ObjectBase.cs
@@ -7,6 +7,10 @@ using System.Xml.Serialization;
 
 namespace RightScale.netClient.Core
 {
+    /// <summary>
+    /// Object base for RightScale API 1.0-based objects
+    /// </summary>
+    /// <typeparam name="T">Type of object - used for deserialization processes</typeparam>
     public class RightScaleAPI10ObjectBase<T>
     {
         public string href;
@@ -20,51 +24,51 @@ namespace RightScale.netClient.Core
         #region serializer methods
 
         /// <summary>
-        /// Method deserializes a Json string into a single instance of the derived class defined by <typeparamref name="T"/>
+        /// Method deserializes a XML string into a single instance of the derived class defined by <typeparamref name="T"/>
         /// </summary>
-        /// <param name="jsonString">Json String to deserialize into an instance of <typeparamref name="T"/></param>
+        /// <param name="xmlString">XML String to deserialize into an instance of <typeparamref name="T"/></param>
         /// <returns>Instance of type <typeparamref name="T"/></returns>
-        protected static T deserialize(string jsonString)
+        protected static T deserialize(string xmlString)
         {
-            return objectDeserialize(jsonString);
+            return objectDeserialize(xmlString);
         }
 
         /// <summary>
-        /// Method deserializes a Json string into a generic list of objects of the derived class defined by <typeparamref name="T"/>
+        /// Method deserializes a XML string into a generic list of objects of the derived class defined by <typeparamref name="T"/>
         /// </summary>
-        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <param name="xmlString">XML string to deserialize into a list of <typeparamref name="T"/></param>
         /// <returns>Generic list of <typeparamref name="T"/></returns>
-        protected static List<T> deserializeList(string jsonString)
+        protected static List<T> deserializeList(string xmlString)
         {
-            return listDeserialize(jsonString);
+            return listDeserialize(xmlString);
         }
 
         /// <summary>
-        /// Method deserializes a Json string into a single instance of the derived class defined by <typeparamref name="T"/>
+        /// Method deserializes a XML string into a single instance of the derived class defined by <typeparamref name="T"/>
         /// This method is accessible by other classes in this library for classes that return collections of other types of classes
         /// </summary>
-        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <param name="xmlString">XML string to deserialize into a list of <typeparamref name="T"/></param>
         /// <returns></returns>
-        internal static T populateObjectFromJson(string jsonString)
+        internal static T populateObjectFromJson(string xmlString)
         {
-            return objectDeserialize(jsonString);
+            return objectDeserialize(xmlString);
         }
 
         /// <summary>
-        /// Method deserializes a Json string into a generic list of objects of the derived class defined by <typeparamref name="T"/>
+        /// Method deserializes a XML string into a generic list of objects of the derived class defined by <typeparamref name="T"/>
         /// This method is accessible by other classes in this library for classes that return collections of other types of classes
         /// </summary>
-        /// <param name="jsonString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <param name="xmlString">XML string to deserialize into a list of <typeparamref name="T"/></param>
         /// <returns></returns>
-        internal static List<T> populateObjectListFromJson(string jsonString)
+        internal static List<T> populateObjectListFromJson(string xmlString)
         {
-            return listDeserialize(jsonString);
+            return listDeserialize(xmlString);
         }
 
         /// <summary>
-        /// Centralized private method for handling deserialization of Json into the proper generic list of type defined by <typeparamref name="T"/>
+        /// Centralized private method for handling deserialization of XML into the proper generic list of type defined by <typeparamref name="T"/>
         /// </summary>
-        /// <param name="xmlString">Json string to deserialize into a list of <typeparamref name="T"/></param>
+        /// <param name="xmlString">XML string to deserialize into a list of <typeparamref name="T"/></param>
         /// <returns>Generic list of type <typeparamref name="T"/></returns>
         private static List<T> listDeserialize(string xmlString)
         {
@@ -87,9 +91,9 @@ namespace RightScale.netClient.Core
         }
 
         /// <summary>
-        /// Centralized private method for handling deserialization of Json into the proper type defined by <typeparamref name="T"/>
+        /// Centralized private method for handling deserialization of XML into the proper type defined by <typeparamref name="T"/>
         /// </summary>
-        /// <param name="xmlString">Json string to deserizlize into an instance of <typeparamref name="T"/></param>
+        /// <param name="xmlString">XML string to deserizlize into an instance of <typeparamref name="T"/></param>
         /// <returns>Instance of type <typeparamref name="T"/></returns>
         private static T objectDeserialize(string xmlString)
         {
