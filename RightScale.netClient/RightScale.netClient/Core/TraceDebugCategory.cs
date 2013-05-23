06c0e05c8e96d9b05d87d47d39e6b338537c6825
Joshua McLean <joshua.mclean@rightscale.com>
2013-05-23 09:00:25 -0700

    Updating

---
+++ RightScale.netClient/RightScale.netClient/Core/TraceDebugCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightScale.netClient.Core
+{
+    /// <summary>
+    /// Collection of strings used within the Debug.Write/line/if process to identify categories
+    /// </summary>
+    public static class TraceDebugCategory
+    {
+        public const string HttpGet = "Http GET";
+        public const string HttpPut = "Http PUT";
+        public const string HttpPost = "Http POST";
+        public const string HttpDelete = "Http DELETE";
+        public const string RSAPIAuthenticate = "RS API Authentication";
+    }
+}
